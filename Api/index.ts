9ee98ee01b7c38a4b0edd6da8e06ab1a212d40b6
Christian <christian.nader@decathlon.com>
2021-12-11 14:19:52 +0100

    basic implementation

---
+++ Api/index.ts
@@ -0,0 +1,7 @@
+import { TodoApi, Todo } from './generated';
+
+export {
+    TodoApi,
+    Todo
+};
+
--- App.tsx
+++ App.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {TodoApi} from "./Api";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+
+    const [data, setData] = useState([]);
+
+    let fetchData = async () => {
+        const todoApi = new TodoApi();
+        let response = await todoApi.getTodos().then((response) => response.data)
+        setData([response.at(0).name, response.at(0).date, response.at(0).description]);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <Button title="get Todos List" onPress={fetchData}/>
+            <Text>{data.at(0)}</Text>
+            <Text>{data.at(1)}</Text>
+            <Text>{data.at(2)}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
