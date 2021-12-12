5c83c54a70abe3ba0e9183ccb18cdc69bc4b7f5c
Christian <christian.nader@decathlon.com>
2021-12-12 17:15:14 +0100

    get todo by id

--- App.tsx
+++ App.tsx
@@ -4,20 +4,33 @@ import Api from './Api';
 
 export default function App() {
 
-    const [data, setData] = useState([]);
+    const [todosList, setTodosList] = useState([]);
 
-    let fetchData = async () => {
+    const [inputText, setInputText] = useState('');
+
+    let fetchTodos = async () => {
         let response = await Api.TodoApi.getTodos().then((response) => response.data)
-        setData([response.at(0).name, response.at(0).date, response.at(0).description]);
+        setTodosList([response.at(0).name, response.at(0).date, response.at(0).description]);
+    }
+
+    let fetchTodo = async () => {
+        let response = await Api.TodoApi.getTodo(inputText).then((response) => response.data)
+        alert(response.name);
+    }
+
+    let captureInput = (input) => {
+        setInputText(input);
     }
 
     return (
         <View style={styles.container}>
             <Text>Open up App.tsx to start working on your app!</Text>
-            <Button title="get Todos List" onPress={fetchData}/>
-            <Text>{data.at(0)}</Text>
-            <Text>{data.at(1)}</Text>
-            <Text>{data.at(2)}</Text>
+            <Button title="get Todos List" onPress={fetchTodos}/>
+            <Text>{todosList.at(0)}</Text>
+            <Text>{todosList.at(1)}</Text>
+            <Text>{todosList.at(2)}</Text>
+            <input placeholder="pet id to get" onChange={captureInput}/>
+            <Button title="get todo by id" onPress={fetchTodo}/>
         </View>
     );
 }
